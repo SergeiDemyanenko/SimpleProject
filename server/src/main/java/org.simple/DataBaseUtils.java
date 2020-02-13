67ab71c6856c6f9fec49d187b77fdf1882330770
TSSoftware-hub <sergey.trukhanov@outlook.com>
2020-02-12 19:59:09 -0800

    SIM-20: Added new method api/list-it, added new class ToDoItem.java

--- server/src/main/java/org.simple/DataBaseUtils.java
+++ server/src/main/java/org.simple/DataBaseUtils.java
@@ -14,6 +14,8 @@ public class DataBaseUtils {
 
     private static List<String> todoList = null;
 
+    private static List<ToDoItem> todoListIT = null;
+
     private static Properties getProps() {
         Properties result = new Properties();
         URL url = DataBaseUtils.class.getClassLoader().getResource("application.properties");
@@ -53,10 +55,35 @@ public class DataBaseUtils {
         return todoList;
     }
 
-    public static void deleteRecord(String id) throws SQLException {
+    public static List<ToDoItem> getTodoListIT() throws SQLException {
+        if (todoListIT == null) {
+            todoListIT = new ArrayList<>();
+
+            Connection conn = getConnect();
+            Statement sql_stmt = conn.createStatement();
+            ResultSet rset = sql_stmt.executeQuery("SELECT id, text FROM todo_list");
+            while (rset.next()) {
+                todoListIT.add(new ToDoItem(rset.getInt("id"), rset.getString("text")));
+            }
+        }
+
+        return todoListIT;
+    }
+
+    public static void deleteRecord(Integer id) throws SQLException {
         Connection conn = getConnect();
         PreparedStatement stmt = conn.prepareStatement("DELETE FROM todo_list WHERE id = ?");
-        stmt.setString(1,id);
+        stmt.setInt(1,id);
+        stmt.execute();
+
+        todoListIT.remove(id);
+    }
+
+    public static void editRecord(int id, String text) throws SQLException {
+        Connection conn = getConnect();
+        PreparedStatement stmt = conn.prepareStatement("UPDATE todo_list SET text = ? WHERE id = ?");
+        stmt.setString(1, text);
+        stmt.setInt(2, id);
         stmt.execute();
     }
 }
---
+++ server/src/main/java/org.simple/ToDoItem.java
@@ -0,0 +1,12 @@
+package org.simple;
+
+public class ToDoItem {
+
+    private int id;
+    private String text;
+
+    public ToDoItem(int id, String text) {
+        this.id = id;
+        this.text = text;
+    }
+}
