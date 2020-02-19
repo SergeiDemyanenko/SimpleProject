f14da8913b0359e0c1b39bfba316018e11c570ab
Sharygin-Aleksei <mcmorattichannel@gmail.com>
2020-02-18 17:23:15 -0800

    Fix test

--- server/src/test/java/org/simple/IntegrationTests.java
+++ server/src/test/java/org/simple/IntegrationTests.java
@@ -21,7 +21,7 @@ import java.sql.*;
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.util.AssertionErrors.assertNotNull;
+import static org.springframework.test.util.AssertionErrors.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(
