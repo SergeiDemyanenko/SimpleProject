3a9f741f174acf51492308e1e9baae8b6c6ca949
Sharygin-Aleksei <mcmorattichannel@gmail.com>
2020-02-19 15:40:43 -0800

    Refactor test to Page Object

--- ui-tests/src/test/java/org/simple/UITest.java
+++ ui-tests/src/test/java/org/simple/UITest.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -38,19 +37,17 @@ public class UITest {
     }
 
     @Test
-    public void deleteTest() throws InterruptedException {
+    public void deleteTest(){
         WebDriver driver = new ChromeDriver();
-        WebDriverWait wait = new WebDriverWait(driver, 5);
+        MainPage mainPage = new MainPage(driver);
         try {
             driver.get("http://localhost:3000/");
 
-            List<WebElement> todoList = wait.until(presenceOfAllElementsLocatedBy(By.xpath("//span[@class='todo-list-item']")));
-            assertTrue("There are nothing to delete in the List",todoList.size() > 0);
+            int todoListSize = mainPage.getAllTodoItems().size();
+            assertTrue("There are nothing to delete in the List", todoListSize > 0);
 
-            todoList.get(0).findElement(By.xpath("//i[@class='fa fa-trash-o']")).click();
-            List<WebElement> newTodoList = wait.until(presenceOfAllElementsLocatedBy(By.xpath("//span[@class='todo-list-item']")));
-
-            assertEquals(todoList.size() - 1, newTodoList.size());
+            mainPage.deleteFirstTodo();
+            assertEquals(todoListSize - 1, mainPage.getAllTodoItems().size());
         } finally {
             driver.close();
         }
