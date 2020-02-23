34d4956366160fa266f2b0169bc46fc188ea03d3
TSSoftware-hub <sergey.trukhanov@outlook.com>
2020-02-23 11:43:44 -0800

    SIM-31: Updated data.sql

--- server/src/test/resources/data.sql
+++ server/src/test/resources/data.sql
@@ -1,7 +1,13 @@
-INSERT INTO TODO_LIST (text) VALUES
-  ('go to'),
-  ('do something'),
-  ('look at it');
+INSERT INTO TODO_LIST (text, group_id) VALUES
+  ('go to settings', 2),
+  ('go to online store', 1),
+  ('delete unwanted items from your shopping cart', 1),
+  ('change your home address', 2),
+  ('save changes', 2),
+  ('buy some goods', 1),
+  ('go to shopping cart', 3),
+  ('check your shopping cart', 1),
+  ('finish shopping', 3);
 
 INSERT INTO TODO_GROUP (group_name) VALUES
   ('Group 1'),
