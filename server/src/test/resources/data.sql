8f7d2a3e5f8e51247ca32f7a2bd2bc05f5f70fe8
TSSoftware-hub <sergey.trukhanov@outlook.com>
2020-02-23 11:03:41 -0800

    SIM-31: Added new table TODO_GROUP, altered table TODO_LIST (added field group_id, linked to TODO_GROUP id field). Added data to the data.sql file

--- server/src/test/resources/data.sql
+++ server/src/test/resources/data.sql
@@ -2,3 +2,8 @@ INSERT INTO TODO_LIST (text) VALUES
   ('go to'),
   ('do something'),
   ('look at it');
+
+INSERT INTO TODO_GROUP (group_name) VALUES
+  ('Group 1'),
+  ('Group 2'),
+  ('Group 3');
