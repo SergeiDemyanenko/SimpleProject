dcc292748588ce5d7991ddb837c65db77c39119d
Sergey Demyanenko <sdemyanenko@pinterest.com>
2020-03-10 17:18:00 -0700

    selenium grid, step 3

---
+++ docker/wait-for-grid.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# wait-for-grid.sh
+
+set -e
+
+cmd="$@"
+
+while ! curl -sSL "http://localhost:4444/wd/hub/status" 2>&1 \
+        | grep "\"ready\": true," >/dev/null; do
+    echo 'Waiting for the Grid'
+    sleep 1
+done
+
+>&2 echo "Selenium Grid is up"
