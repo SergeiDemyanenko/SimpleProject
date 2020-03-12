78cd889fad4b9a2bd25174bc5f5617fd29343581
Sergey Demyanenko <sdemyanenko@pinterest.com>
2020-03-11 18:51:34 -0700

    selenium grid, step 3.9

--- docker/wait-for-grid.sh
+++ docker/wait-for-grid.sh
@@ -3,14 +3,17 @@
 
 set -e
 
-if [ -z "$HOST_IP" ]; then
-    echo "variable HOST_IP has to be defined"
-else
-    while ! curl -sSL "http://localhost:4444/wd/hub/status" 2>&1 \
+seconds=10
+while ! curl -sSL "http://localhost:4444/wd/hub/status" 2>&1 \
              | grep "\"ready\": true," >/dev/null; do
-            echo 'Waiting for the Grid'
-         sleep 1
-    done
+    echo 'Waiting for the Grid'
+    sleep 1
+    seconds=$(($seconds - 1))
 
-    >&2 echo "Selenium Grid is up"
-fi
+    if [ $seconds -le 0 ]; then
+        echo "Time out"
+        exit 1
+    fi
+done
+
+echo "Selenium Grid is up"
