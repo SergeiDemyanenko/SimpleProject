21e22ce7d2612b806e087c3c373cb89e50f8fdfb
Aleksei Sharygin <52227211+Sharygin-Aleksei@users.noreply.github.com>
2020-03-12 19:25:23 -0700

    Update UITest.java

--- server/src/test/java/org/simple/ui/UITest.java
+++ server/src/test/java/org/simple/ui/UITest.java
@@ -33,6 +33,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfAllElementsLocatedBy;
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+import static org.springframework.test.util.AssertionErrors.assertNotNull;
 
 
 @RunServer
