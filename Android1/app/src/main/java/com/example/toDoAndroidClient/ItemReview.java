945e47c7af687926caa596904ea573b7dd7092d8
Aleks Grig <aleks032118@gmail.com>
2020-03-22 02:38:38 -0700

    Android client, initially commit Клиент работает автономно Выбранные цвета элементов...Я художник, я так вижу ))) (на самом деле я очень плохо различаю цвета между собой)

---
+++ Android1/app/src/main/java/com/example/toDoAndroidClient/ItemReview.java
@@ -0,0 +1,71 @@
+package com.example.toDoAndroidClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.myapplication102.R;
+
+public class ItemReview extends AppCompatActivity {
+
+    private String itemText;
+    private int itemId;
+    private TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_review);
+
+        textView = (TextView) findViewById(R.id.textReviewView);
+        itemText = getIntent().getExtras().get(MainActivity.NEW_ITEM_TEXT).toString();
+        itemId = Integer.parseInt(getIntent().getExtras().get(MainActivity.ITEM_ID).toString());
+        textView.setText(itemText);
+    }
+
+
+    public void editItem(View view) {
+
+        Intent intent = new Intent(this, TextEditorActivity.class);
+
+
+        intent.putExtra(MainActivity.EDIT_TEXT, itemText);
+        //  intent.putExtra(MainActivity.ITEM_ID, itemId);
+
+        startActivityForResult(intent, itemId);
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == itemId) {
+
+            try {
+                String textOfEditItem = data.getStringExtra(MainActivity.NEW_ITEM_TEXT);
+                MainActivity.taskList.set(itemId, textOfEditItem);
+                //          adapter.add(textOfNewItem);
+                textView.setText(textOfEditItem);
+                MainActivity.adapter.notifyDataSetChanged();
+            } catch (NullPointerException e) {
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+
+    public void deleteItem(View view) {
+
+        MainActivity.taskList.remove(itemId);
+        MainActivity.adapter.notifyDataSetChanged();
+        finish();
+
+    }
+
+
+}
+
+
