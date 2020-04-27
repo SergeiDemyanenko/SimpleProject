f2462ff35d412da556e8f6f1812e3bb744dea2f6
Sharygin-Aleksei <mcmorattichannel@gmail.com>
2020-04-26 21:35:55 -0700

    added enable + disable auth by BOOLEAN in application.properties

--- server/src/main/java/org.simple/SecurityConfigurer.java
+++ server/src/main/java/org.simple/SecurityConfigurer.java
@@ -4,18 +4,25 @@ package org.simple;
 import org.simple.Helpers.JwtRequestFilter;
 import org.simple.Helpers.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
+@Configuration
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+    @Value("${application.authorization.enabled}")
+    private Boolean authorizationEnabled;
+
+    @Value("${application.authorization.redirect}")
+    private String authorizationRedirect;
 
     @Autowired
     private MyUserDetailsService myUserDetailsService;
@@ -30,14 +37,25 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests().antMatchers("/h2", "/h2/", "/api/login", "/api/signUp").permitAll()
-                .anyRequest().authenticated()
-                .and().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-    }
 
+        if (Boolean.FALSE.equals(authorizationEnabled)) {
+            http.csrf().disable()
+                    .authorizeRequests().antMatchers("/**").permitAll()
+                    .anyRequest().authenticated();
+        }
+
+        if (Boolean.TRUE.equals(authorizationEnabled))
+         {
+             http.authorizeRequests()
+                     .antMatchers(AuthController.LOGIN).permitAll()
+                     .antMatchers(AuthController.SIGNUP).permitAll()
+                     .antMatchers("/resources/**").permitAll()
+                     .antMatchers("/*.js").permitAll()
+                     .anyRequest().authenticated()
+                     .and()
+                     .formLogin().loginPage(authorizationRedirect);
+         }
+    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
