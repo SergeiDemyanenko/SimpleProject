26befd717562585ab4566c418dfe50f73ff1a98a
Sharygin-Aleksei <mcmorattichannel@gmail.com>
2020-05-03 23:43:38 -0700

    Added Groups API + API TEST for groups

--- server/src/main/java/org.simple/entity/ToDoGroup/ToDoGroupRepository.java
+++ server/src/main/java/org.simple/entity/ToDoGroup/ToDoGroupRepository.java
@@ -2,9 +2,15 @@ package org.simple.entity.ToDoGroup;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface ToDoGroupRepository extends JpaRepository<ToDoGroup, Long> {
 
+    ToDoGroup findById(long id);
+
     List<ToDoGroup> findAllByOrderById();
+
+    @Transactional
+    void deleteById(Long id);
 }
--- server/src/main/java/org/simple/Controller.java
+++ server/src/main/java/org/simple/Controller.java
@@ -21,37 +21,52 @@ public class Controller {
     @Autowired
     private ToDoGroupRepository toDoGroupRepository;
 
-    @RequestMapping("/list-obj")
+    @RequestMapping(method = RequestMethod.GET, value = "/list-obj")
     public List<ToDoItem> listObj() {
         return toDoItemRepository.findAll();
     }
 
-    @RequestMapping("/list")
+    @RequestMapping(method = RequestMethod.GET, value = "/list")
     public List<String> list() {
         return toDoItemRepository.getToDoTexts();
     }
 
-    @RequestMapping("/list-group")
-    public List<ToDoGroup> listGroup() {
+    @RequestMapping(method = RequestMethod.GET, value = "/list-group")
+    public List<ToDoGroup> listGroup(){
         return toDoGroupRepository.findAllByOrderById();
     }
 
-    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST, value = "/add")
     public ToDoItem add(@RequestBody ToDoItem toDoItem) {
         return toDoItemRepository.save(toDoItem);
     }
 
-    @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+    @RequestMapping(method = RequestMethod.DELETE, value = "/delete")
     public void delete(@RequestParam Long id) {
         toDoItemRepository.deleteById(id);
     }
 
-    @RequestMapping(value = "/edit", method = RequestMethod.PATCH)
-    public ToDoItem edit(@RequestBody ToDoItem toDoItem) {
-        return toDoItemRepository.save(toDoItem);
+    @RequestMapping(method = RequestMethod.PATCH, value = "/edit")
+    public void edit(@RequestBody ToDoItem toDoItem){
+        toDoItemRepository.save(toDoItem);
     }
 
-    @RequestMapping("/hello")
+    @RequestMapping(method = RequestMethod.POST, value = "/addGroup")
+    public ToDoGroup addGroup(@RequestBody ToDoGroup toDoGroup){
+        return toDoGroupRepository.save(toDoGroup);
+    }
+
+    @RequestMapping(method = RequestMethod.PATCH, value = "/editGroup")
+    public void editGroup(@RequestBody ToDoGroup toDoGroup){
+        toDoGroupRepository.save(toDoGroup);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/deleteGroup")
+    public void deleteGroup(@RequestParam Long id){
+        toDoGroupRepository.deleteById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/hello")
     public String hello() {
         return "hello";
     }
